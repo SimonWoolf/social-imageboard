91520f9c2b83571fd8332bb9ee64a6344bab72fc
Simon Woolf <simon@simonwoolf.net>
2014-01-20 16:34:15 +0000

    secret token use env variables

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module InstagramClone
   class Application < Rails::Application
+    config.secret_token = ENV['SECRET_TOKEN']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.secret_token = ENV['SECRET_TOKEN']
