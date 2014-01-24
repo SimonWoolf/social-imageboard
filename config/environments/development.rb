21148b5ec7d786df506d690ba9a0656689db6369
Simon Woolf <simon@simonwoolf.net>
2014-01-24 11:25:18 +0000

    devise model create

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # devise setup suggests adding this:
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
