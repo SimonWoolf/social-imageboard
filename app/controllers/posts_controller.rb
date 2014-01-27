e24f611849027d6cbcd96a4dbbfb1a9e1bd51684
Simon Woolf <simon@simonwoolf.net>
2014-01-27 11:07:53 +0000

    refactored find_post in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   POST_FIELDS = [:title, :body, :image, :tagstring]
-  
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -14,22 +14,24 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(params[:post].permit(POST_FIELDS))
+    Post.create(params[:post].permit(POST_FIELDS))
     redirect_to '/posts'
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(params[:post].permit(POST_FIELDS))
     redirect_to action: :show
   end
 
   def destroy
-    Post.find(params[:id]).destroy
+    @post.destroy
     redirect_to action: :index
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
