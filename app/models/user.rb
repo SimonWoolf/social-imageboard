68f8cb52fc228c9e4e6dffddddf2a4701f21ac1f
Simon Woolf <simon@simonwoolf.net>
2014-03-17 14:24:30 +0000

    user not recoverable (not done email...)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   has_many :posts
   has_many :upvotes
